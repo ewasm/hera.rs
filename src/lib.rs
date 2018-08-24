6810529803ca83287293dbd2942d343f2388e7f2
Alex Beregszaszi <alex@rtfs.hu>
2018-08-24 22:06:18 +0100

    working demo

--- src/lib.rs
+++ src/lib.rs
@@ -4,15 +4,24 @@ use std::ffi::CString;
 //#[derive(Default)]
 pub struct evmc_instance {
     abi_version: i32,
-    name: CString,
-    version: CString,
+    name: *const u8,
+    version: *const u8,
     destroy: extern fn(c: *mut evmc_instance),
+    execute: extern fn(c: *mut evmc_instance),
+    set_tracer: extern fn(c: *mut evmc_instance),
+    set_option: extern fn(c: *mut evmc_instance)
 //    execute: *u8,
 //    set_tracer: *u8,
 //    set_option: *u8
 }
 
 extern fn evmc_destroy(instance: *mut evmc_instance) {
+  println!("destroy called");
+  panic!()
+}
+
+extern fn evmc_execute(instance: *mut evmc_instance) {
+  println!("execute called");
   panic!()
 }
 
@@ -21,18 +30,27 @@ impl evmc_instance {
     }
     fn new() -> Self {
       evmc_instance{
-          abi_version: 3,
-          name: CString::new("a").unwrap(),
-          version: CString::new("b").unwrap(),
-          destroy: evmc_destroy
+          abi_version: 5,
+          name: "hera.rs\0".as_ptr(), //CString::new("a").unwrap(),
+          version: "0.0.0\0".as_ptr(), //CString::new("b").unwrap(),
+          destroy: evmc_destroy,
+          execute: evmc_execute,
+          set_tracer: evmc_destroy,
+          set_option: evmc_destroy
       }
     }
 }
 
 #[no_mangle]
 pub extern fn evmc_create() -> Box<evmc_instance> {
+    println!("evmc_create called...");
 //    Box::new(evmc_instance{ abi_version: 3, name: CString::new("a").unwrap(), version: CString::new("b").unwrap()  })
     Box::new(evmc_instance::new())
+}
+
+#[no_mangle]
+pub extern fn evmc_create_hera() -> Box<evmc_instance> {
+    evmc_create()
 }
 
 #[cfg(test)]
