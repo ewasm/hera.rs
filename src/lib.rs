341a2348ba37e0ca7295140061d16d2b1cedbdc6
Alex Beregszaszi <alex@rtfs.hu>
2018-08-25 01:15:23 +0100

    proper memory ownership

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,18 @@ include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 extern fn evmc_destroy(instance: *mut evmc_instance) {
     println!("destroy called");
+    if !instance.is_null() { unsafe { Box::from_raw(instance); } }
+}
+
+extern fn evmc_release_result(result: *const evmc_result) {
+    println!("releasing result");
+    unsafe {
+        if !(*result).output_data.is_null() {
+            // Must typecast here since evmc.h defines it as const,
+            // so that consumers do not try to modify it.
+            Box::from_raw((*result).output_data as *mut u8);
+        }
+    }
 }
 
 extern fn evmc_execute(
@@ -17,22 +29,27 @@ extern fn evmc_execute(
     code_size: usize,
 ) -> evmc_result {
     println!("execute called");
-    let ret = evmc_result {
-        status_code: -1,
+
+    let output_data = [0u8;69];
+    let output_size = 69;
+
+    let output_data = Box::into_raw(Box::new(output_data));
+
+    evmc_result {
+        status_code: 0,
         gas_left: 0,
-        output_data: 0 as *const u8,
-        output_size: 0,
-        release: None,
+        output_data: output_data as *const u8,
+        output_size: output_size,
+        release: Some(evmc_release_result),
         create_address: evmc_address { bytes: [0u8;20] },
         padding: [0u8;4]
-    };
-    ret
+    }
 }
 
 #[no_mangle]
-pub extern fn evmc_create() -> evmc_instance {
+pub extern "system" fn evmc_create() -> *mut evmc_instance {
     println!("evmc_create called...");
-    evmc_instance {
+    let ret = evmc_instance {
       abi_version: EVMC_ABI_VERSION as i32,
       name: "hera.rs\0".as_ptr() as *const i8,
       version: "0.0.0\0".as_ptr() as *const i8,
@@ -40,7 +57,8 @@ pub extern fn evmc_create() -> evmc_instance {
       execute: Some(evmc_execute),
       set_option: None,
       set_tracer: None
-    }
+    };
+    Box::into_raw(Box::new(ret))
 //    Box::new(evmc_instance{ abi_version: 3, name: CString::new("a").unwrap(), version: CString::new("b").unwrap()  })
 //    evmc_instance::new()
 //    let ptr = ::std::ptr::null_mut();
@@ -49,6 +67,6 @@ pub extern fn evmc_create() -> evmc_instance {
 }
 
 #[no_mangle]
-pub extern fn evmc_create_hera() -> evmc_instance {
+pub extern "system" fn evmc_create_hera() -> *mut evmc_instance {
     evmc_create()
 }
